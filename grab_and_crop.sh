a5d24b72c7805bdb85ab79496a6533f5636af1c4
Chris Gathercole <chris.gathercole@ft.com>
2016-07-28 11:51:41 +0100

    MODIFIED: extracting banner bounds from XML

--- grab_and_crop.sh
+++ grab_and_crop.sh
@@ -7,6 +7,8 @@ BANNER_CROP_HEIGHT=1000
          CROPS_DIR=./crops
      ORIGINALS_DIR=./originals
     ANIMATIONS_DIR=./animations
+BOUNDS_ARTICLES_DIR=./bounds_articles
+  BOUNDS_BANNER_DIR=./bounds_banner
    ANIMATION_DELAY=1
          FRAMERATE=10
       SCALED_WIDTH=1080
@@ -16,6 +18,8 @@ BANNER_CROP_HEIGHT=1000
 mkdir -p $CROPS_DIR
 mkdir -p $ORIGINALS_DIR
 mkdir -p $ANIMATIONS_DIR
+mkdir -p $BOUNDS_ARTICLES_DIR
+mkdir -p $BOUNDS_BANNER_DIR
 
 MP4_BASE_NAME=$1
 
@@ -45,10 +49,15 @@ if [[ "$MP4_BASE_NAME" = "" ]]; then # for now, any arg means skip creating crop
 				                   YMD=$(printf "%4d%02d%02d" $YEAR $MONTH $DAY)
 				       IMAGE_BASE_NAME=$(printf "FTDA-%4d-%02d%02d-0001" $YEAR $MONTH $DAY)
 				     SOURCE_IMAGE_NAME=${IMAGE_BASE_NAME}.JPG
-				      SOURCE_IMAGE_URL=$ARCHIVE_BASE_URL/all_years/$YEAR/$YMD/$SOURCE_IMAGE_NAME
+				  SOURCE_URL_BASE_PATH=$ARCHIVE_BASE_URL/all_years/$YEAR/$YMD
+				      SOURCE_IMAGE_URL=$SOURCE_URL_BASE_PATH/$SOURCE_IMAGE_NAME
 				 SOURCE_IMAGE_FILENAME=$ORIGINALS_DIR/$SOURCE_IMAGE_NAME
 				    CROPPED_IMAGE_NAME=${IMAGE_BASE_NAME}-cropped-to-banner.JPG
 				CROPPED_IMAGE_FILENAME=$CROPS_DIR/$CROPPED_IMAGE_NAME
+				         XML_BASE_NAME=$(printf "FTDA-%4d-%02d%02d" $YEAR $MONTH $DAY)
+				        SOURCE_XML_URL=$SOURCE_URL_BASE_PATH/${XML_BASE_NAME}.xml
+		 	  BOUNDS_ARTICLES_FILENAME=$BOUNDS_ARTICLES_DIR/${XML_BASE_NAME}.bounds_articles.psv
+		 	    BOUNDS_BANNER_FILENAME=$BOUNDS_BANNER_DIR/${XML_BASE_NAME}.bounds_banner.txt
 
 				if [ -e $SOURCE_IMAGE_FILENAME ]; then
 					echo "skipping WGET of $SOURCE_IMAGE_FILENAME: local copy exists"
@@ -61,10 +70,65 @@ if [[ "$MP4_BASE_NAME" = "" ]]; then # for now, any arg means skip creating crop
 					if [[ -e $CROPPED_IMAGE_FILENAME ]]; then
 						echo "skipping convert of $CROPPED_IMAGE_FILENAME: local copy exists"
 					else
+						if [ -s $BOUNDS_ARTICLES_FILENAME ]; then
+							echo "skipping WGET of XML: $BOUNDS_ARTICLES_FILENAME exists"
+						else
+							wget -O- $SOURCE_XML_URL | perl -ne '
+/<imdim>([^<]+)<\/imdim>/ and $imdim=$1;
+/<da>([^<]+)<\/da>/ and $da=$1;
+/<dw>([^<]+)<\/dw>/ and $dw=$1;
+/<ti>([^<]+)<\/ti>/ and $ti=$1; 
+/<article type="([^"]+)">/ and $atype=$1;
+if (/<id>([^<]+)<\/id>/) {
+	$id=$1;
+	@pieces = split(/-/, $id);
+	if (scalar(@pieces) == 5) {
+		if ($pieces[3] ne "0001") { 
+			last;
+		}
+	} else {
+		$mainId=$id
+	}
+}
+/<ct>([^<]*)<\/ct>/ and $ct=$1;
+/<pg [^>]+ pos="(.*)"\/>/ and print join("|", $mainId, $da, $dw, $imdim, $atype, $ct, $id, $ti, $1), "\n"
+' > $BOUNDS_ARTICLES_FILENAME 
+						fi
+
+						if [ -s $BOUNDS_BANNER_FILENAME ]; then
+							echo "skipping calc of banner bounds: $BOUNDS_BANNER_FILENAME exists"
+						else
+							cat $BOUNDS_ARTICLES_FILENAME | perl -ne '
+BEGIN {
+	$mostLeft  = 1000000;
+	$mostRight = 0;
+	$mostUp    = 100000;
+	$mostDown  = 0;
+	$mostDownFrontMatter = 0;
+}
+@p = split(/\|/, $_); 
+$ti = $p[5];
+@coords = split(/,/, $p[8]);
+if($coords[0] < $mostLeft ) { $mostLeft  = $coords[0] };
+if($coords[1] < $mostUp   ) { $mostUp    = $coords[1] };
+if($coords[2] > $mostRight) { $mostRight = $coords[2] };
+if($coords[3] > $mostDown ) { $mostDown  = $coords[3] };
+if( $ti =~ /Front matter/i and $coords[3] < 1000 ){
+	if($coords[3] > $mostDownFrontMatter){ $mostDownFrontMatter = $coords[3]};
+}
+END {
+	print sprintf("%dx%d+%d+%d", ($mostRight - $mostLeft), ($mostDownFrontMatter - $mostUp), $mostLeft, $mostUp);
+}
+' > $BOUNDS_BANNER_FILENAME
+						fi
+
+						BOUNDS=$(cat $BOUNDS_BANNER_FILENAME)
+
 						convert \
-							-crop "${BANNER_CROP_WIDTH}x${BANNER_CROP_HEIGHT}+0+0" \
+							-crop "$BOUNDS" \
 							$SOURCE_IMAGE_FILENAME \
 							$CROPPED_IMAGE_FILENAME
+							# -crop "${BANNER_CROP_WIDTH}x${BANNER_CROP_HEIGHT}+0+0" \
 					fi
 				fi
 			done
